d967ee435413fb26157fba4adc7970f3f55ad46a
Seth <seth@powerloom.io>
2025-04-30 14:56:41 +0530

    refactor: update RPC configuration model and dependencies
    - Removed unused RPC configuration classes from `settings_model.py` to streamline the codebase.
    - Updated the `poetry.lock` file to reflect the new resolved reference for the `rpc-helper` dependency.

--- utils/models/settings_model.py
+++ utils/models/settings_model.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List, Optional, Union
+from rpc_helper.utils.models.settings_model import RPCConfigFull
+from typing import List, Union
 
 class HTTPXConfig(BaseModel):
     """HTTPX client configuration model."""
@@ -50,33 +51,6 @@ class Redis(BaseModel):
     cluster_mode: bool = False
     data_retention: RedisDataRetentionConfig
 
-
-class RPCNodeConfig(BaseModel):
-    """RPC node configuration model."""
-    url: str
-    rate_limit: RateLimitConfig
-
-
-class ConnectionLimits(BaseModel):
-    """Connection limits configuration model."""
-    max_connections: int = 100
-    max_keepalive_connections: int = 50
-    keepalive_expiry: int = 300
-
-
-class RPCConfigBase(BaseModel):
-    """Base RPC configuration model."""
-    full_nodes: List[RPCNodeConfig]
-    archive_nodes: Optional[List[RPCNodeConfig]]
-    force_archive_blocks: Optional[int]
-    retry: int
-    request_time_out: int
-    connection_limits: ConnectionLimits
-
-class RPCConfigFull(RPCConfigBase):
-    """Full RPC configuration model."""
-    polling_interval: float
-    semaphore_value: int = 20
 
 class Preloader(BaseModel):
     """Preloader configuration model."""
